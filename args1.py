7c304b9634eecc10e4cdd6634026c839343e7332
fusekushal <kushal.bhattarai@fusemachines.com>
2023-08-03 15:34:06 +0545

    Programs after implementing docstring in functions

--- args1.py
+++ args1.py
@@ -1,11 +1,11 @@
 def add(*num):
+    '''function to implement the concept of awrgs by adding given arbitrary numbers'''
     sum = 0
-    
     for n in num:
         sum = sum + n
-
     return(sum)
 
+print(add.__doc__)
 print(add(3, 5, 6, 7, 8))
 print(add(4,5,6,3,2,3,1,3,3,3,4,5,5,5,2))
 print(add(5.3, 5.4, 2.4, 2))
---
+++ args2.py
@@ -0,0 +1,10 @@
+def strccat(*str):
+    '''function to implement the concept of awrgs by concatnating arbitrary numbers of string'''
+    fstr = " "
+    for n in str:
+        fstr = fstr + n
+    return(fstr)
+print(strccat.__doc__)
+print(strccat("Kushal", " Bhattarai"))
+print(strccat("I", " live", " in", " Bhaisepati"))
+print(strccat("I"," Love"," One"," Piece"))
